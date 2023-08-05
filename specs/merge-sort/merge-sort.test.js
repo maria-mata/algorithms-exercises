d70444c441e690396a78de5665660c63889de958
Maria Mata <25022665+maria-mata@users.noreply.github.com>
2023-08-05 15:54:21 -0600

    Recursion: merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -8,11 +8,39 @@
 
 const mergeSort = (nums) => {
   // code goes here
+  if (nums.length < 2) return nums;
+
+  const middle = Math.floor(nums.length / 2);
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle);
+
+  const leftSorted = mergeSort(left);
+  const rightSorted = mergeSort(right);
+
+  return merge(leftSorted, rightSorted);
 };
+
+// left and right are already sorted
+function merge(left, right) {
+  const sorted = [];
+
+  // breaks when one of the arrays is empty
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      // pushes the first element, while also removing it from the original
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+
+  // one of the lists will be empty, but the other will not so we concatenate everything
+  return sorted.concat(left, right);
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
