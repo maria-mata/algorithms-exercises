0147d6277d4c4ce83e9c12e0a078a6199c0fa98f
Maria Mata <25022665+maria-mata@users.noreply.github.com>
2023-08-05 13:06:31 -0600

    Insertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -15,11 +15,24 @@
 
 function insertionSort(nums) {
   // code goes here
+  for (let i = 1; i < nums.length; i++) {
+    let current = nums[i];
+    let j;
+
+    // right to left
+    for (j = i - 1; nums[j] > current && j >= 0; j--) {
+      nums[j + 1] = nums[j]; // move to right
+    }
+
+    // insert
+    nums[j + 1] = current;
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
