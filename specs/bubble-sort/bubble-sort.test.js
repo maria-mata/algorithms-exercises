be548c21e672bffa828ede142d867288c4b472c6
Maria Mata <25022665+maria-mata@users.noreply.github.com>
2023-08-04 20:49:25 -0600

    Bubble sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,13 +10,32 @@
   index's. Continue looping through until all values are in ascending order
 */
 
-function bubbleSort(nums) {
-  // code goes here
+// O(n^2) - time complexity
+// O(1) - space complexity
+function bubbleSort(nums, stop = null) {
+  const stopIndex = stop || nums.length - 1;
+  let swapCount = 0;
+
+  for (let i = 0; i < stopIndex; i++) {
+    const current = nums[i];
+    const next = nums[i + 1];
+    if (current > next) {
+      nums[i] = next;
+      nums[i + 1] = current;
+      swapCount += 1;
+    }
+  }
+
+  if (swapCount > 0) {
+    return bubbleSort(nums, stopIndex - 1);
+  } else {
+    return nums;
+  }
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
